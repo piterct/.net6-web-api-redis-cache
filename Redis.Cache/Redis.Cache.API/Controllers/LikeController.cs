b5c0b31d66960c70d782e8dcc2ce9c7b502996f6
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-15 18:11:42 -0300

    Initial Like controller

---
+++ Redis.Cache/Redis.Cache.API/Controllers/LikeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Redis.Cache.API.Commands;
+
+namespace Redis.Cache.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class LikeController : Controller
+    {
+        private readonly ILogger<LikeController> _logger;
+
+        public LikeController(ILogger<LikeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Post(CreateLikeCommand command)
+        {
+            try
+            {
+                return Ok(command);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError("An exception has occurred at {dateTime}. " +
+                 "Exception message: {message}." +
+                 "Exception Trace: {trace}", DateTime.UtcNow, exception.Message, exception.StackTrace);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
