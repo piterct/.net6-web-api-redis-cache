902ebc96bee999a193809701c956831e802fd9e7
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-28 23:20:05 -0300

    Like Controller

--- Redis.Cache/Redis.Cache.API/Controllers/LikeController.cs
+++ Redis.Cache/Redis.Cache.API/Controllers/LikeController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Redis.Cache.API.Commands;
+using Redis.Cache.Application.Inrterfaces.Repositories;
+using Redis.Cache.Application.Models;
 
 namespace Redis.Cache.API.Controllers
 {
@@ -10,10 +12,31 @@ namespace Redis.Cache.API.Controllers
     public class LikeController : Controller
     {
         private readonly ILogger<LikeController> _logger;
+        private readonly ILikeRepository _likeRepository;
 
-        public LikeController(ILogger<LikeController> logger)
+        public LikeController(ILogger<LikeController> logger, ILikeRepository likeRepository)
         {
             _logger = logger;
+            _likeRepository = likeRepository;
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Post(Guid id)
+        {
+            try
+            {
+                var like = await _likeRepository.Get(id);
+                return Ok(like);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError("An exception has occurred at {dateTime}. " +
+                 "Exception message: {message}." +
+                 "Exception Trace: {trace}", DateTime.UtcNow, exception.Message, exception.StackTrace);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpPost]
@@ -22,7 +45,8 @@ namespace Redis.Cache.API.Controllers
         {
             try
             {
-                return Ok(command);
+                var like = await _likeRepository.Add(new Like(command.Name));
+                return Ok(like);
             }
             catch (Exception exception)
             {
