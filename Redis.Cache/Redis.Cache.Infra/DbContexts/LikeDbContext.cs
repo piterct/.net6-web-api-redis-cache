2415ce1978d250199769c248936d2093682b0cad
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-01-28 23:20:30 -0300

    LikeDbContext

---
+++ Redis.Cache/Redis.Cache.Infra/DbContexts/LikeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Redis.Cache.Application.Models;
+
+namespace Redis.Cache.Infra.DbContexts
+{
+    public class LikeDbContext : DbContext
+    {
+        public LikeDbContext(DbContextOptions<LikeDbContext> options) : base(options) { }
+
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Like>()
+                .HasKey(t => t.Id);
+
+            builder.Entity<Like>().Property(x => x.Name)
+           .IsRequired();
+        }
+    }
+}
