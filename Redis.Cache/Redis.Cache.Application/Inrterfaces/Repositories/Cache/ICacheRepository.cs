f3fc14857ae9bbb91d393980956e907c1e225bf7
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-02-01 20:15:18 -0300

    Cache Repository

---
+++ Redis.Cache/Redis.Cache.Application/Inrterfaces/Repositories/Cache/ICacheRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Redis.Cache.Application.Inrterfaces.Repositories.Cache
+{
+    public interface ICacheRepository
+    {
+        Task<T?> GetValue<T>(Guid id);
+        Task<IEnumerable<T?>?> GetColletion<T>(string colletionKey);
+        Task SetValue<T>(Guid id, T obj);
+        Task SetColletion<T>(string collectionKey, IEnumerable<T> colletion);
+    }
+}
