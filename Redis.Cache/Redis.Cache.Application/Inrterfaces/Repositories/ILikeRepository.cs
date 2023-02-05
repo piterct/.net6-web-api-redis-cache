943ee71fb3acb82c9cbbe87ef1bf09ad30ed37a7
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-02-05 12:14:28 -0300

    ILikeRepository

---
+++ Redis.Cache/Redis.Cache.Application/Inrterfaces/Repositories/ILikeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Redis.Cache.Application.Models;
+
+namespace Redis.Cache.Application.Inrterfaces.Repositories
+{
+    public interface ILikeRepository : IRepository
+    {
+        Task<Like> Add(Like like);
+        Task<Like> Get(Guid id);
+    }
+}
