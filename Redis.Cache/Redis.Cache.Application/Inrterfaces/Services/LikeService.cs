807d2e98ec2e1fffdf76facc4ddd71d5a554ae71
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-02-12 21:38:28 -0300

    Like Service

---
+++ Redis.Cache/Redis.Cache.Application/Inrterfaces/Services/LikeService.cs
@@ -0,0 +1,19 @@
+﻿using Redis.Cache.Application.Inrterfaces.Repositories;
+using Redis.Cache.Application.Inrterfaces.Repositories.Cache;
+
+namespace Redis.Cache.Application.Inrterfaces.Services
+{
+    public class LikeService : ILikeService
+    {
+        private readonly ILikeRepository _likeRepository;
+        private readonly ICacheRepository _cacheRepository;
+        private const string CACHE_COLLETION_KEY = "_AllLikes";
+
+        public LikeService(ILikeRepository likeRepository, ICacheRepository cacheRepository)
+        {
+            _likeRepository = likeRepository;
+            _cacheRepository = cacheRepository;
+        }
+    } 
+}
+ 
