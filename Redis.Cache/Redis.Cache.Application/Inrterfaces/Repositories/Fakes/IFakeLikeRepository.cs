bc1b28cd5efd2c1820b927ed0f6a2ef8cfd6b37a
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-02-18 16:21:58 -0300

    IFakeRepository

---
+++ Redis.Cache/Redis.Cache.Application/Inrterfaces/Repositories/Fakes/IFakeLikeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Redis.Cache.Application.Models;
+
+namespace Redis.Cache.Application.Inrterfaces.Repositories.Fakes
+{
+    public interface IFakeLikeRepository
+    {
+        Task<IEnumerable<Like>> GetLikes();
+    }
+}
