50b31fb650ce5c103e763d88fbe98f6d28c83a94
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-02-19 12:48:41 -0300

    Like Models

---
+++ Redis.Cache/Redis.Cache.Application/Models/Like.cs
@@ -0,0 +1,14 @@
+﻿namespace Redis.Cache.Application.Models
+{
+    public class Like
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+
+        public Like(string name)
+        {
+            Id= Guid.NewGuid();
+            Name= name;
+        }
+    }
+}
