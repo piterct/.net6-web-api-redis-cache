d749ceb941ee0c10abba4abc77a8d1c09033b4ab
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-02-19 12:48:22 -0300

    FakeLikeRepository

---
+++ Redis.Cache/Redis.Cache.Infra/Repositories/Fakes/FakeLikeRepository.cs
@@ -0,0 +1,19 @@
+﻿using Redis.Cache.Application.Inrterfaces.Repositories.Fakes;
+using Redis.Cache.Application.Models;
+
+namespace Redis.Cache.Infra.Repositories.Fakes
+{
+    public  class FakeLikeRepository : IFakeLikeRepository
+    {
+
+        public async Task<IEnumerable<Like>> GetLikes()
+        {
+            return await Task.FromResult(GenerateFakeLikes());
+        }
+
+        private IEnumerable<Like> GenerateFakeLikes()
+        {
+            return Enumerable.Range(1, 20).Select(index => new Like($"Like {index}"));
+        }
+    }
+}
