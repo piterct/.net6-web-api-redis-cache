293b952fa2143cb1e2bd26bbef83666e300850df
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-02-20 01:11:32 -0300

    ILikeService

--- Redis.Cache/Redis.Cache.Application/Inrterfaces/Services/ILikeService.cs
+++ Redis.Cache/Redis.Cache.Application/Inrterfaces/Services/ILikeService.cs
@@ -1,6 +1,10 @@
-﻿namespace Redis.Cache.Application.Inrterfaces.Services
+﻿using Redis.Cache.Application.Models;
+
+namespace Redis.Cache.Application.Inrterfaces.Services
 {
     public interface ILikeService
     {
+        Task<Like?> GetLike(Guid id);
+        Task<IEnumerable<Like?>> GetLikes();
     }
 }
