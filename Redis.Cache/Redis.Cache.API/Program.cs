cbf6550ff50bef578b6dbc33267e2da8e43d8810
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-05-07 18:23:29 -0300

    all inviromment are able to see swagger

--- Redis.Cache/Redis.Cache.API/Program.cs
+++ Redis.Cache/Redis.Cache.API/Program.cs
@@ -51,11 +51,10 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsProduction())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.MapControllers();
