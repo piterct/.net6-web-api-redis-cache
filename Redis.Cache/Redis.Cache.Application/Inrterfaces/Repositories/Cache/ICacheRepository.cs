6139c2be6a7f646164e9d601dfb3f06d05ae15d7
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-06-19 09:24:07 -0300

    RemoveAsync

--- Redis.Cache/Redis.Cache.Application/Inrterfaces/Repositories/Cache/ICacheRepository.cs
+++ Redis.Cache/Redis.Cache.Application/Inrterfaces/Repositories/Cache/ICacheRepository.cs
@@ -6,5 +6,6 @@
         Task<IEnumerable<T?>?> GetColletion<T>(string colletionKey);
         Task SetValue<T>(Guid id, T obj);
         Task SetColletion<T>(string collectionKey, IEnumerable<T> colletion);
+        Task RemoveAsync(Guid id);
     }
 }
