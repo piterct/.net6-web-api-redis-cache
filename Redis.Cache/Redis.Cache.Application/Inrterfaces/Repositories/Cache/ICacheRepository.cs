4101df6270772dd5918b12d78bf6cf89c3bd2644
Michael Peter <42197306+piterct@users.noreply.github.com>
2023-08-06 20:24:39 -0300

    parameters  absoluteExpirationRelativeToNow and slidingExpiration

--- Redis.Cache/Redis.Cache.Application/Inrterfaces/Repositories/Cache/ICacheRepository.cs
+++ Redis.Cache/Redis.Cache.Application/Inrterfaces/Repositories/Cache/ICacheRepository.cs
@@ -4,8 +4,8 @@
     {
         Task<T?> GetValue<T>(Guid id);
         Task<IEnumerable<T?>?> GetColletion<T>(string colletionKey);
-        Task SetValue<T>(Guid id, T obj);
-        Task SetColletion<T>(string collectionKey, IEnumerable<T> colletion);
+        Task SetValue<T>(Guid id, T obj, int absoluteExpirationRelativeToNow = 300, int slidingExpiration = 300);
+        Task SetColletion<T>(string collectionKey, IEnumerable<T> colletion, int absoluteExpirationRelativeToNow = 300, int slidingExpiration = 300);
         Task RemoveAsync(Guid id);
     }
 }
